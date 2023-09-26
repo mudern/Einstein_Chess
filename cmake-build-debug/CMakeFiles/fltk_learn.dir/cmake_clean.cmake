1741098b5c5f7482578e5e51b2c94201c5f4408a
mudern <2826778618@qq.com>
2023-09-26 22:20:25 +0800

    Delete file "ChessPiece.cpp" Submitted by MuYanxin on 2023-09-26 22:21:27

---
+++ cmake-build-debug/CMakeFiles/fltk_learn.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fltk_learn.dir/main.cpp.obj"
+  "CMakeFiles/fltk_learn.dir/main.cpp.obj.d"
+  "CMakeFiles/fltk_learn.dir/src/ChessPiece/ChessPiece.cpp.obj"
+  "CMakeFiles/fltk_learn.dir/src/ChessPiece/ChessPiece.cpp.obj.d"
+  "fltk_learn.exe"
+  "fltk_learn.exe.manifest"
+  "fltk_learn.pdb"
+  "libfltk_learn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fltk_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
